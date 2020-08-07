693ac51206b1413df66a812264cd215c1be384af
Tim Lai <tlai4@disroot.org>
2020-08-07 17:53:30 -0400

    Full D diagonal search

--- scalar_diagonal_elements.py
+++ scalar_diagonal_elements.py
@@ -11,6 +11,7 @@ to find the possible subgroups in the scalar embedding case.
 import collections
 import json
 from functools import reduce
+from itertools import product
 
 def compare(list1,list2):
     return collections.Counter(list1) == collections.Counter(list2)
@@ -30,8 +31,8 @@ generators = {1: [1], 2: [1], 3: [2], 4: [3], 5: [2], 6: [5], 7: [3], 8: [3,5],
               21: [2,20], 22: [7], 23: [5], 24: [5,7,13], 25: [2], 26: [7], 
               27: [2], 28: [3,13], 29: [2], 30: [7,11], 31: [3], 32: [3,31], 
               33: [2,10], 34: [3], 35: [2,6], 36: [5,19], 42: [5, 13], 
-              48: [5, 7, 47], 54: [5], 63: [2, 5], 72: [5, 7, 19], 
-              84: [5, 11, 13], 108: [5, 107], 126: [5, 13]}
+              48: [5, 7, 47], 54: [5], 56: [3, 13, 29], 63: [2, 5], 72: [5, 7, 19], 
+              84: [5, 11, 13], 96: [5, 7, 31], 108: [5, 107], 126: [5, 13]}
 # Consider the scalar embedding case.
 # Classify all rational elements of the form 
 #(\zeta_n^a, \zeta_n^b, \zeta_n^{-a-b}).
@@ -99,12 +100,12 @@ empty_keys = [k for k,v in C.items() if len(v) == 0]
 for k in empty_keys:
     del C[k]
 # Analyze AF
-D = {}
+AF = {}
 for n in C:
     for d in range(1, 4):
         k = 2*d*n
         R = generators[k]
-        D[(n, d)] = []
+        AF[(n, d)] = []
         for a, b, in C[n]:
             for r in range(d):
                 x = 6*n*r + 2*a*d - 2*b*d - d*n
@@ -113,23 +114,23 @@ for n in C:
                 inv = [(x)%k, (-x)%k, (y)%k, (-y)%k, (z)%k, (-z)%k]
                 flag = True
                 for gen in R:
-                    r_inv = [(gen*w) for w in inv]
+                    r_inv = [(gen*w)%k for w in inv]
                     if not compare(inv, r_inv):
                         flag = False
                         break
                 if flag:
-                    D[(n, d)].append([a, b, r])
+                    AF[(n, d)].append([a, b, r])
 
 # Using the solutions for AF, let's analyze arbitrary words comprised of AF.
-empty_keys = [k for k,v in D.items() if len(v) == 0]
-D_final = {}
+empty_keys = [k for k,v in AF.items() if len(v) == 0]
+AF_final = {}
 for k in empty_keys:
-    del D[k]    
-for n, d in D:
+    del AF[k]    
+for n, d in AF:
     k = 2*d*n
     R = generators[k]
-    D_final[(n, d)] = []
-    for a, b, r in D[(n, d)]:
+    AF_final[(n, d)] = []
+    for a, b, r in AF[(n, d)]:
         flag = True
         for f in range(n):
             for g in range(d):
@@ -138,49 +139,112 @@ for n, d in D:
                 z = f*(4*b*d + 2*a*d)
                 inv = [(x)%k, (-x)%k, (y)%k, (-y)%k, (z)%k, (-z)%k]
                 for gen in R:
-                    r_inv = [(gen*w) for w in inv]
+                    r_inv = [(gen*w)%k for w in inv]
                     if not compare(inv, r_inv):
                         flag = False
                         break
         if flag:
-            D_final[(n, d)].append([a, b, r])
+            AF_final[(n, d)].append([a, b, r])
 
-# AG'
-AG = {}
-for d in range(1, 4):
-    k = 2*d
+empty_keys = [k for k,v in AF_final.items() if len(v) == 0]
+for k in empty_keys:
+    del AF_final[k]
+D_diag = {}
+for n, d in AF_final:
+    k = 2*d*n
     R = generators[k]
-    AG[d] = []
-    for r in range(d):
-        x = 5*r - d
-        y = 5*r - 2*d
-        inv = [(x)%k, (-x)%k, (y)%k, (-y)%k, (d)%k, (-d)%k]
+    D_diag[(n, d)] = []
+    for [a, b, r], s in product(AF_final[(n, d)], range(d)):
         flag = True
-        for gen in R:
-            r_inv = [(gen*w)%k for w in inv]
-            if not compare(inv,r_inv):
-                flag = False
-                break
-        if flag:
-            AG[d].append(r) 
+        for etas in product(range(n), repeat = 4):
+            for deltas in product(range(d), repeat = 4):
+                eta_one = 2*d*n*etas[0] - 2*d*(etas[1]+etas[3])*(a+b) + 2*a*d*etas[2]
+                eta_two = 2*a*d*etas[1] + 2*b*d*(etas[0]+etas[3]) - 2*d*etas[2]*(a+b)
+                eta_three = 2*a*d*etas[3] + 2*b*d*(etas[1]+etas[2]) - 2*d*etas[0]*(a+b)
             
-# FG'
-FG = {}
-for n in C:
-    k = 2*n
-    R = generators[k]
-    FG[n] = []
-    for a, b in C[n]:
-        x = 3*a - n
-        y = 3*a - 2*n
-        inv = [(x)%k, (-x)%k, (y)%k, (-y)%k, (n)%k, (-n)%k]
-        flag = True
-        for gen in R:
-            r_inv = [(gen*w)%k for w in inv]
-            if not compare(inv,r_inv):
-                flag = False
-                break
+                delta_one = 4*n*r*deltas[0] + (8*r+4*s)*n*deltas[1] + (d-2*r)*n*deltas[2] + (d-2*r-s)*n*deltas[3]
+                delta_two = (d-2*r)*n*deltas[0] + (d-2*r-s)*n*deltas[1] + 4*n*r*deltas[2] + (8*r+4*s)*n*deltas[3]
+                delta_three = (d-2*r)*n*deltas[0] + (d-2*r-s)*n*deltas[1]+ (d-2*r)*n*deltas[2] + (d-2*r-s)*n*deltas[3]
+                    
+                x = (eta_one + delta_one) - (eta_two + delta_two)
+                y = (eta_one + delta_one) - (eta_three + delta_three)
+                z = eta_two + delta_two - (eta_three + delta_three)
+                    
+                inv = [(x)%k, (-x)%k, (y)%k, (-y)%k, (z)%k, (-z)%k]
+                for gen in R:
+                    r_inv = [(gen*w)%k for w in inv]
+                    if not compare(inv, r_inv):
+                        flag = False
+                        break        
         if flag:
-            FG[n].append([a, b])             
+            D_diag[(n, d)].append([a, b, r, s])
+
+
+                
+                
+# G = {}
+# empty_keys = [k for k,v in AF_final.items() if len(v) == 0]
+# for k in empty_keys:
+#     del AF_final[k] 
+# for n, d in AF_final:
+#     k = 2*d*n
+#     R = generators[k]
+#     G[(n, d)] = []
+#     for a, b, r in AF_final[(n, d)]:
+#         flag = True
+#         for f in range(n):
+#             for g in range(d):
+#                 for s in range(d):
+#                     x = 6*n*r*g + 2*a*d*f*g - 2*n*(2*r+s) - d*n*g + 2*a*d*f + 2*b*d*f
+#                     y = 6*n*r*g + 2*a*d*f*g + 2*n*(2*r+s) - d*n*g - 2*b*d*f
+#                     z = 4*n*(2*r+s) - 2*a*d*f - 4*b*d*f
+#                     inv = [(x)%k, (-x)%k, (y)%k, (-y)%k, (z)%k, (-z)%k]
+#                     for gen in R:
+#                         r_inv = [(gen*w) for w in inv]
+#                         if not compare(inv, r_inv):
+#                             flag = False
+#                             break
+#         if flag:
+#             G[(n, d)]. append([a, b, r, s])
+# AG'
+# AG = {}
+# for d in range(1, 4):
+#     k = 2*d
+#     R = generators[k]
+#     AG[d] = []
+#     for r in range(d):
+#         x = 6*r - d
+#         y = 6*r - 2*d
+#         inv = [(x)%k, (-x)%k, (y)%k, (-y)%k, (d)%k, (-d)%k]
+#         flag = True
+#         for gen in R:
+#             r_inv = [(gen*w)%k for w in inv]
+#             if not compare(inv,r_inv):
+#                 flag = False
+#                 break
+#         if flag:
+#             AG[d].append(r) 
+            
+# AFG = {}
+# empty_keys = [k for k,v in AF_final.items() if len(v) == 0]
+# for k in empty_keys:
+#     del AF_final[k] 
+# for n, d in AF_final:
+#     k = 4*d*n
+#     R = generators[k]
+#     AFG[(n, d)] = []
+#     for a, b, r in AF_final[(n, d)]:
+#         flag = True
+#         x = 6*n*r + 2*a*d
+#         y = 6*n*r - 2*n*d + 2*a*d
+#         z = -2*n*d
+#         inv = [(x)%k, (-x)%k, (y)%k, (-y)%k, (z)%k, (-z)%k]
+#         for gen in R:
+#             r_inv = [(gen*w)%k for w in inv]
+#             if not compare(inv, r_inv):
+#                 flag = False
+#                 break
+#         if flag:
+#             AFG[(n, d)].append([a, b, r])
 
             
